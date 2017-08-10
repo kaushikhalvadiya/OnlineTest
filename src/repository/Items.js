8b1d99a7b11bcd7585e7c48b370c42fb9476623a
kaushikhalvadiya <kaushikhalvadiya@yahoo.co.in>
2017-08-10 15:06:23 +0530

    Added Item repository that will contains all products with its properties

---
+++ src/repository/Items.js
@@ -0,0 +1,28 @@
+module.exports = {
+    Products: [
+        {
+            "Pid":1,
+            "Name":"Printer",
+            "SKU":"DJKS526",
+            "Price":20
+        },
+        {
+            "Pid":2,
+            "Name":"Scanner",
+            "SKU":"DJKS527",
+            "Price":25
+        },
+        {
+            "Pid":3,
+            "Name":"Mouse",
+            "SKU":"DJKS528",
+            "Price":22
+        },
+        {
+            "Pid":4,
+            "Name":"Key board",
+            "SKU":"DJKS529",
+            "Price":28
+        }
+    ]
+}
